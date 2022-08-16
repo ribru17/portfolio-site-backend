6952b4d8c83b32d08e6bcb54c6009a8bd1c7a9c8
Riley B <ribru17@hotmail.com>
2022-08-16 11:05:52 -0700

    initial

---
+++ dist/server.js
@@ -0,0 +1,53 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var dotenv_1 = __importDefault(require("dotenv"));
+var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var path_1 = __importDefault(require("path"));
+var nodemailer_1 = __importDefault(require("nodemailer"));
+// dotenv.config({path: __dirname + './../.env'}) // starts from 'dist' folder
+dotenv_1.default.config({ path: path_1.default.join(__dirname, './../.env') }); // starts from 'dist' folder
+var PORT = process.env.PORT || 8000;
+var app = (0, express_1.default)();
+console.log('h');
+var transporter = nodemailer_1.default.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.EMAIL_PW,
+    }
+});
+transporter.verify(function (error, success) {
+    if (error) {
+        console.log(error);
+    }
+});
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+// app.use(express.static(path.join(__dirname, '../../client/build')))
+app.post('/api/contact', function (req, res) {
+    var mail = {
+        from: process.env.EMAIL_ADDRESS,
+        to: process.env.EMAIL_ADDRESS,
+        subject: "Web message from ".concat(req.body.name),
+        text: req.body.message
+    };
+    transporter.sendMail(mail, function (err, data) {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Failed to send message");
+        }
+        else {
+            res.status(200).send("Successfully sent message");
+        }
+    });
+});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../../client/build/index.html'))
+// })
+app.listen(PORT, function () {
+    console.log("listening on ".concat(PORT));
+});
---
+++ server.ts
@@ -0,0 +1,54 @@
+import dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import path from 'path'
+import nodemailer from 'nodemailer'
+import Mail from 'nodemailer/lib/mailer'
+
+// dotenv.config({path: __dirname + './../.env'}) // starts from 'dist' folder
+dotenv.config({path: path.join(__dirname, './../.env')}) // starts from 'dist' folder
+const PORT = process.env.PORT || 8000
+const app = express()
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_PW,
+    }
+})
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.log(error)
+    }
+})
+
+app.use(cors())
+app.use(express.json())
+// app.use(express.static(path.join(__dirname, '../../client/build')))
+
+app.post('/api/contact', (req, res) => {
+    const mail: Mail.Options = {
+        from: process.env.EMAIL_ADDRESS,
+        to: process.env.EMAIL_ADDRESS,
+        subject: `Web message from ${req.body.name}`,
+        text: req.body.message
+    }
+    transporter.sendMail(mail, (err, data) => {
+        if (err) {
+            console.log(err)
+            res.status(500).send("Failed to send message")
+        } else {
+            res.status(200).send("Successfully sent message")
+        }
+    })
+})
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../../client/build/index.html'))
+// })
+
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
